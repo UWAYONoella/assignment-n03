dddc95893363793dd06ecc00edc82ee6f226cce7
UWAYONoella <noellawayo2019@gmail.com>
2022-09-27 00:30:13 +0200

    Feat:Assignment

---
+++ study.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>study</title>
+</head>
+<body>
+    <?php
+    $color="red";
+    // $code="me"
+    echo"My car is".$color."<br>";
+    // echo"My car is".$code."<br>";
+    // echo"My car is".$COLOR."<br>"
+    // echo"My car is".$coLOR."<br>"
+     $x=5;
+     echo "<p>variable inside function is $x</p>";
+     echo "<br>";
+     $y=45.78;
+     var_dump($y);
+     $car=array("volvo","toyota","BMW");
+     var_dump($car);
+     $a="hello world";
+     $a=null;
+     var_dump($a);
+     echo strlen("hello word");
+    ?>
+</body>
+</html>
